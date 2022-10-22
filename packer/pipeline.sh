71bee5dbbeba8e514ae82069640f33956abe1d9a
Luciano Sampaio <lsampaioweb@gmail.com>
2022-10-22 19:55:31 -0300

    Fixed the name of the build.

--- packer/pipeline.sh
+++ packer/pipeline.sh
@@ -3,4 +3,4 @@ set -e # Abort if there is an issue with any build.
 
 packer build -only="credencials.file.password" .
 packer build -only="machine.proxmox-iso.template" .
-packer build -only="pve-node.null.extra-config" .
+packer build -only="kvm-node.null.extra-config" .
