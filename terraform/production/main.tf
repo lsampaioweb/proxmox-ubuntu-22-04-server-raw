b32fdbc131accb60d6d61377d21e277612d94de8
Luciano Sampaio <lsampaioweb@gmail.com>
2022-10-31 23:53:56 -0300

    Added the terraform specific files for each folder/project.

--- terraform/production/main.tf
+++ terraform/production/main.tf
@@ -1,6 +1,8 @@
 module "proxmox-ubuntu-22-04" {
   source = "../modules/proxmox-ubuntu-22-04"
 
-  clone = "ubuntu-22-04-server-raw"
-  name  = "ubuntu-22-04-server-raw-vm-production"
+  clone       = "ubuntu-22-04-server-raw"
+  name        = "ubuntu-22-04-server-raw-vm-production"
+  description = "Ubuntu 22.04 VM with bare minimum settings"
+  pool        = "Production"
 }
--- terraform/stagging/main.tf
+++ terraform/stagging/main.tf
@@ -1,6 +1,8 @@
 module "proxmox-ubuntu-22-04" {
   source = "../modules/proxmox-ubuntu-22-04"
 
-  clone = "ubuntu-22-04-server-raw"
-  name  = "ubuntu-22-04-server-raw-vm-stagging"
+  clone       = "ubuntu-22-04-server-raw"
+  name        = "ubuntu-22-04-server-raw-vm-stagging"
+  description = "Ubuntu 22.04 VM with bare minimum settings"
+  pool        = "Stagging"
 }
