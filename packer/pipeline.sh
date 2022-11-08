1f1f1c1424c647f246f1a8e84062f18a07d185f3
Luciano Sampaio <lsampaioweb@gmail.com>
2022-11-08 00:04:20 -0300

    Removed all the code that was here and now is in the packer-proxmox-ubuntu-22-04-iso repository.

--- packer/pipeline.sh
+++ packer/pipeline.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e # Abort if there is an issue with any build.
 
-packer build -only="credencials.file.password" .
-packer build -only="machine.proxmox-iso.template" .
-packer build -only="kvm-node.null.extra-config" .
+packer init config.pkr.hcl
+packer build -only="credencials.file.password" -var-file=project.pkrvars.hcl iso/.
+packer build -only="template.proxmox-iso.ubuntu" -var-file=project.pkrvars.hcl iso/.
+packer build -only="kvm-node.null.extra-config" -var-file=project.pkrvars.hcl iso/.
