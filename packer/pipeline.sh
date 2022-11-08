3360137a49983a9af8db3e3ebb3851a9654f6273
Luciano Sampaio <lsampaioweb@gmail.com>
2022-11-08 00:09:56 -0300

    Added one space at the end of the line.

--- packer/pipeline.sh
+++ packer/pipeline.sh
@@ -2,6 +2,6 @@
 set -e # Abort if there is an issue with any build.
 
 packer init config.pkr.hcl
-packer build -only="credencials.file.password" -var-file=project.pkrvars.hcl iso/.
-packer build -only="template.proxmox-iso.ubuntu" -var-file=project.pkrvars.hcl iso/.
-packer build -only="kvm-node.null.extra-config" -var-file=project.pkrvars.hcl iso/.
+packer build -only="credencials.file.password" -var-file=project.pkrvars.hcl iso/. 
+packer build -only="template.proxmox-iso.ubuntu" -var-file=project.pkrvars.hcl iso/. 
+packer build -only="kvm-node.null.extra-config" -var-file=project.pkrvars.hcl iso/. 
