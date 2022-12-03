383e1879c03a2e90aa436e45cc4aac80a9f6a9fc
Luciano Sampaio <lsampaioweb@gmail.com>
2022-12-03 08:14:38 -0300

    Updated the name of the source so it can be more generic.

--- packer/pipeline.sh
+++ packer/pipeline.sh
@@ -3,5 +3,5 @@ set -e # Abort if there is an issue with any build.
 
 packer init config.pkr.hcl
 packer build -only="credencials.file.password" -var-file=project.pkrvars.hcl iso/. 
-packer build -only="template.proxmox-iso.ubuntu" -var-file=project.pkrvars.hcl iso/. 
+packer build -only="ubuntu.proxmox-iso.template" -var-file=project.pkrvars.hcl iso/. 
 packer build -only="kvm-node.null.extra-config" -var-file=project.pkrvars.hcl iso/. 
