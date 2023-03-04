495763a8471ec6e09f4601913ef15e2ddbb4435e
Luciano Sampaio <lsampaioweb@gmail.com>
2023-03-04 15:42:52 -0300

    Cloud-init works.

--- links.md
+++ links.md
@@ -1,3 +1,5 @@
+1. [Proxmox Cloud-Init Support.](https://pve.proxmox.com/wiki/Cloud-Init_Support)
+
 1. [Automation with Hashicorp Packer.](https://www.youtube.com/watch?v=tbv1lTF1wFU&list=PL8VzFQ8k4U1Jp6eWgHSXHiiRWRvPyCKRj)
 
 1. [HashiCorp Packer - Build automated machine images.](https://www.udemy.com/course/hashicorp-packer-build-automated-machine-images/)
---
+++ packer/pkr.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e # Abort if there is an issue with any build.
+
+# Usage:
+#   ./pkr.sh validate staging
+#   $1 -> validate, build.
+#   $2 -> staging, production.
+
+# Initialize (download) the packer plugins.
+packer init config.pkr.hcl
+
+# Format all the current and child folders.
+packer fmt -recursive .
+
+packer $1 -only="credencials.file.password" -var-file=$2/vars.pkrvars.hcl iso/. 
+
+packer $1 -only="ubuntu.proxmox-iso.template" -var-file=$2/vars.pkrvars.hcl iso/. 
+
+packer $1 -only="kvm-node.null.extra-config" -var-file=$2/vars.pkrvars.hcl iso/. 
